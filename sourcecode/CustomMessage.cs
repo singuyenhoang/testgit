3cccb6970b17bbda262bf3fd1ec43e581cf909e4
nguyen.hoang <nguyen.hoang@evizi.com>
2018-10-29 15:14:23 +0700

    Update dev on 696

--- sourcecode/CustomMessage.cs
+++ sourcecode/CustomMessage.cs
@@ -14,7 +14,7 @@ namespace WorkflowCore.Sample02.Steps
 
         public override ExecutionResult Run(IStepExecutionContext context)
         {
-        	//HELLO TEST GIT 1
+        	//HELLO TEST GIT 696
             Console.WriteLine(Message);
             return ExecutionResult.Next();
         }
